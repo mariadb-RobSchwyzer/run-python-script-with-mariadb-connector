1886e6bfe01e9f30766b6b7aa8190d3d8c668443
Robert Schwyzer <robert.s@hostdime.com>
2020-11-09 18:41:03 -0500

    Added support for secret file for mariadb_pass

--- script.py
+++ script.py
@@ -2,10 +2,16 @@
 import mariadb
 import sys
 import os
+from pathlib import Path
 
 # Pull defaults from OS environment if they exist
 user = os.environ.get('MARIADB_USER', 'connpy_test')
-password = os.environ.get('MARIADB_PASS', 'passwd')
+# Prefer secret for mariadb_pass if a secret exists
+if Path('/run/secrets/mariadb_pass').is_file():
+   with open("/run/secrets/mariadb_pass", "r") as password_file:
+      password = password_file.read()
+else:
+   password = os.environ.get('MARIADB_PASS', 'passwd')
 host = os.environ.get('MARIADB_HOST', 'localhost')
 port = int(os.environ.get('MARIADB_PORT', 3306))
 
